f1295aaf5bf19944a7367bc8aa320ee5924eb8d5
A.J. Uppal <aj8uppal@gmail.com>
2020-09-24 00:32:18 -0400

    server interface, hashing, log-in

--- server/index.js
+++ server/index.js
@@ -1,24 +1,36 @@
 const path = require("path");
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+
+const uri = "mongodb+srv://admin:backendbabes@cluster0.xeso1.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
+let UID = 0;
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 
-app.post('/login', function (req, res) {
-  const username = req.query.username;
-  const password = req.query.password;
-  console.log(req.body);
-  console.log(`Hash: ${bcrypt.hashSync(password, 10)}`)
-  res.send('POST request to the homepage');
-});
+MongoClient.connect(uri, {
+  useUnifiedTopology: true
+}).then(client => {
+  const collection = client.db("backendbabes").collection("users");
+  app.post('/login', function (req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+    const hash = bcrypt.hashSync(password, 10);
+    let response = collection.insertOne({id: UID++, email: email, password_hash: hash});
+    console.log(response);
+    res.send('POST request to the homepage');
+  });
+  app.use(express.static(path.join(__dirname, "..", "build")));
+  app.use(express.static("public"));
 
-app.use(express.static(path.join(__dirname, "..", "build")));
-app.use(express.static("public"));
-
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
-});
-
-
-app.listen(5000, () => {
-    console.log("server started on port 5000");
-});
+  app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+  });
+  app.listen(5000, () => {
+      console.log("server started on port 5000");
+  });
+}).catch(console.error);
