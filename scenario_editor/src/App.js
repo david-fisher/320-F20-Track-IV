159ced18e7bfd880ea70de83aa5eb3126d938fde
Jonathan Julien <jjulien@umass.edu>
2020-09-29 09:43:57 -0400

    Added React-Bootstrap library. This should make it *much* easier to add and use components on the fly 	Also started work on the welcome page. Removed the logo, but I haven't aligned the first button properly. 	Buttons need to be aligned and implemented, and the welcome messaged should be aligned similarly to the wireframe.

--- scenario_editor/src/App.js
+++ scenario_editor/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from 'react-bootstrap';
+
+// function Button(name){
+
+// }
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* I'm leaving this commented out because I don't think we're gonna pull in some random image at the moment. */}
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Welcome Editor! This is currently <i>very</i> unfinished.
         </p>
@@ -19,6 +26,20 @@ function App() {
           Check the Git page to see our progress!
         </a>
       </header>
+
+      <button onclick="window.location='youtube.com'">Youtube</button>
+      <Button 
+        variant="outline-secondary" 
+        href = "https://duckduckgo.com/?q=How+to+make+a+scenario&t=h_&ia=web"
+      >
+        'Create a New Scenario'
+      </Button>{' '}
+      {/* <Button 
+        variant="outline-secondary" href = 
+        "https://duckduckgo.com/?q=How+to+make+a+scenario&t=h_&ia=web"
+      >
+          Edit an Existing Scenario
+      </Button>{' '} */}
     </div>
   );
 }
