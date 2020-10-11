da84c20f204c3bfc9c6591501fd2461b615a2b1a
Jonathan Julien <jjulien@umass.edu>
2020-10-11 18:32:37 -0400

    Removed Server.js Implementation
    I'm not sure of the purpose of this, so Ive edited lines in package.json that seemed to make this project use server.js to start a web application.

--- scenario_editor/src/server.js
+++ scenario_editor/src/server.js
@@ -1,6 +1,9 @@
+//Not sure of this file's purpose. Used for server implementation?
+//Currently I have removed lines in package.json and package-lock.json 
+//that were used to make this work. - Jonathan
 const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname + "/src", 'build')));
 app.listen(PORT);
