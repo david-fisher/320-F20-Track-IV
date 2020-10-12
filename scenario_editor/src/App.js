fa7df2c36dd3d158423d3a17c6481b4f3d562314
Jonathan Julien <jjulien@umass.edu>
2020-10-12 15:07:00 -0400

    rename fix
    The Components folder had a lowercase filename and it needed to be fixed for consistency

--- scenario_editor/src/App.js
+++ scenario_editor/src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './App.css';
 import Login from "./Login/Login"
 import Welcome from './Welcome/Welcome';
-import Nav from './components/Nav';
+import Nav from './Components/Nav';
 import Home from './Home/Home';
 import Introduction from './Introduction/Introduction';
-import Conversations from './components/Conversations';
+import Conversations from './Components/Conversations';
 import PlayerResponses from './PlayerResponses/PlayerResponses';
 import Build from './Build/Build';
 import Conclusions from './Components/Conclusions';
