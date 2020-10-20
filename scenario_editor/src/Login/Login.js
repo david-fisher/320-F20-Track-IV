6e8348109f730edb87f716dc6b24b730874c6cfc
Marcin <marcinknara@gmail.com>
2020-10-20 10:47:35 -0400

    Update page routing and pages (as per last demo)
    -Update the page routing as showcased last demo
    -Remove unneccessary files (Welcome.css and Welcome.js)
    -Fix css for Introduction buttons

--- scenario_editor/src/Login/Login.js
+++ scenario_editor/src/Login/Login.js
@@ -1,54 +1,101 @@
 import React from 'react';
-import './Login.css';
-import Nav from '../Components/Nav'
-import Button from 'react-bootstrap/Button';
 import {Link} from 'react-router-dom';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+// import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
 function Login() {
-  return (
-    <div className="Login">
-    <div className="Login-header">
-        <header>
-            <h1>
-                Ethics-Simulator
-            </h1>
-        </header>
-    </div>
-    <div className="Login-text">
-        <header>
-            <h2>
-                Login
-            </h2>
-            <h5>
-                Please input your username and password:
-            </h5>
-        </header>
-    </div>
-    <div class="Login-Form">
-        <form>
-            <label>
-                Username: 
-                <input type="text" name="name" />
-                Password: 
-                <input type="text" name="username" />
-            </label>
-            {/* <input class="Submit-Button" type="submit" value="Submit" /> */}
-        </form>
-    </div>
-    <div class="Login-Button">
-      <Link to = "/">
-        <Button lg block
-            variant="outline-secondary"
-            // This isn't working right now. Need to figure out how to route from one page to another, 
-            // possibly using the routing option or href like below.
-        >
+    return (
+      <Typography variant="body2" color="textSecondary" align="center">
+        {'Copyright © Ethicism '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
+  
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center'
+
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    misc_text: {
+        color: 'black'
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+      
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+  
+  export default function SignIn() {
+    const classes = useStyles();
+  
+    return (
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Typography component="h1" variant="h5">
             Login
-        </Button>{' '}
-      </Link>
-    </div>
-
-    </div>
-  );
-}
-
-export default Login;
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Link to="/home">
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+              Sign In
+              </Button>
+            </Link>
+            
+          </form>
+        </div>
+        <Box mt={8}>
+          <Login />
+        </Box>
+      </Container>
+    );
+  }
