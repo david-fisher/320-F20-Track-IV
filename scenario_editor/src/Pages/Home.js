7f6d212a6b0216403395c356d6ec6f2085b1bf2b
Marcin <marcinknara@gmail.com>
2020-10-27 18:22:42 -0400

    Update Home.js and remove Home.css
    -Update the Home.js file to include the styling it needs from Home.css
    -Clean up these two files a little bit

--- scenario_editor/src/Pages/Home.js
+++ scenario_editor/src/Pages/Home.js
@@ -2,7 +2,6 @@
 // UPDATED. Could be structured better but NOT NEEDED at the moment
 // ********************************************
 import React, { Component } from 'react';
-import './Home.css';
 import Nav from '../Components/NavHome'
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
@@ -18,15 +17,29 @@ const useStyles = makeStyles((theme) => ({
     marginTop: theme.spacing(1),
     alignItems: 'center',
     display: 'flex',
-    justifyContent: 'center'
-  }
-
+    justifyContent: 'center',
+  },
+  optionButton: {
+    display: 'inline',
+    marginTop: '10px',
+    color: '#881c1c',
+    textAlign: 'center',
+    padding: '3rem',
+    margin: '1%',
+    color: 'white',
+    fontWeight: '500',
+    borderStyle: 'solid',
+    borderColor: 'black',
+    borderWidth: '3px',
+    fontSize: '20px',
+    backgroundColor: '#881c1c',
+    maxWidth: '300px'
+  },
 }));
 
 function Home() {
 
   const classes = useStyles();
-
   return (
     <div >
       <Nav />
@@ -40,23 +53,21 @@ function Home() {
         </div>
       </div>
       <div className={classes.buttonContainer} >
-        <div class="Left-Button">
+        <div>
           <Button
-            size="custom-button-size"
-            variant="outline-secondary"
+            className={classes.optionButton}
           >
             <Link to="/new_scenario">
               Create a New Scenario
             </Link>
           </Button>{' '}
         </div>
-        <div class="Right-Button">
+        <div>
           <Button
-            size="custom-button-size"
-            variant="outline-secondary"
+            className={classes.optionButton}
           >
             <Link to="/dashboard">
-              Go to Existing Scenarios
+              View/Edit Existing Scenarios
             </Link>
           </Button>
         </div>
