90d3a55cc175ada8f3f498958a5bd27a6186ccb4
G-a-B-i <70230822+G-a-B-i@users.noreply.github.com>
2020-12-01 15:30:22 -0500

    Update Calls2.js

--- scenario_editor/src/Components/Calls2.js
+++ scenario_editor/src/Components/Calls2.js
@@ -1,13 +1,13 @@
-// I made this to test and understand sean's code.
 import axios from 'axios';
-import { baseURL } from '../../Constants/Config';
-// Universal fetch request using axios
-export default function universalFetch(
+
+const baseURL = "" // need to change this
+
+export function universalFetch(
     setResponse, // should be the setState from a state set up like: const [state, setState] = useState({data: null, loading: false, error: true})
     endpoint, // endpoint path
-    onError, // optional function to run when call fails
-    onSuccess, // optional function to run when the call is a success
-    options // set up like this options = { headers: {'Authorization': `token`} }
+    onSuccess, // optional function to run when call is a success
+    onError, // optional function to run when the call fails
+    options // set up like this options = { headers: {‘Authorization’: `token`}
 ) {
     console.log('Fetch started');
     setResponse({
@@ -19,7 +19,7 @@ export default function universalFetch(
         .get(`${baseURL}${endpoint}`, options)
         .then((resp) => {
             console.log('Response received');
-            console.log(resp);
+            console.log(resp.data);
             setResponse({
                 data: resp.data,
                 loading: false,
@@ -36,4 +36,77 @@ export default function universalFetch(
             });
             onError && onError(err.message);
         });
+}
+
+export function universalPost(
+    setResponse,
+    endpoint,
+    data, // Data to post
+    onSuccess,
+    onError,
+    options
+) {
+    console.log("Post Started");
+    setResponse({
+        data: null,
+        loading: true,
+        error: null
+    })
+    axios
+        .post(`${baseURL}${endpoint}`, data, options)
+        .then((resp) => {
+            console.log("Response received");
+            console.log(resp.data);
+            setResponse({
+                data: resp.data,
+                loading: false,
+                error: null,
+            });
+            onSuccess && onSuccess(resp.data);
+        })
+        .catch((err) => {
+            console.log(`Post failed with error ${err.message}`);
+            setResponse({
+                data: null,
+                loading: false,
+                error: err.message,
+            });
+            onError && onError(err.message);
+        })
+}
+
+export function universalDelete(
+    setResponse,
+    endpoint,
+    onSuccess,
+    onError,
+    options
+) {
+    console.log("Delete Started");
+    setResponse({
+        data: null,
+        loading: true,
+        error: null
+    })
+    axios
+        .delete(`${baseURL}${endpoint}`, options)
+        .then((resp) => {
+            console.log("Response received");
+            console.log(resp.data);
+            setResponse({
+                data: resp.data,
+                loading: false,
+                error: null,
+            });
+            onSuccess && onSuccess(resp.data);
+        })
+        .catch((err) => {
+            console.log(`Delete failed with error ${err.message}`);
+            setResponse({
+                data: null,
+                loading: false,
+                error: err.message,
+            });
+            onError && onError(err.message);
+        })
 }
