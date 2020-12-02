a96eda8b87d07abe48a73e7b4acce2ef1c2661c7
Marcin <marcinknara@gmail.com>
2020-12-02 14:52:17 -0500

    Changed pageReducer reducers accordingly for updated page type and name

--- scenario_editor/src/Reducers/pagesReducer.js
+++ scenario_editor/src/Reducers/pagesReducer.js
@@ -8,22 +8,22 @@ const pagesReducer = (state = initialPages, action) => {
         // Currently allows only ONE of each page to be added. 
         // Need to find a way to update the page if it exists.
         case 'ADD_INTRODUCTION':
-            console.log(action.payload.type)
-            let introIndex = state.findIndex(item => item.type == action.payload.type);
+            console.log(action.payload.name)
+            let introIndex = state.findIndex(item => item.name == action.payload.name);
             if (introIndex === -1) {
                 return [...state, action.payload]
             }
             return state
         case 'ADD_TASK_ASSIGNMENT':
-            console.log(action.payload.type)
-            let taskIndex = state.findIndex(item => item.type == action.payload.type);
+            console.log(action.payload.name)
+            let taskIndex = state.findIndex(item => item.name == action.payload.name);
             if (taskIndex === -1) {
                 return [...state, action.payload]
             }
             return state
         case 'ADD_CONSEQUENCES':
-            console.log(action.payload.type)
-            let consequencesIndex = state.findIndex(item => item.type == action.payload.type);
+            console.log(action.payload.name)
+            let consequencesIndex = state.findIndex(item => item.name == action.payload.name);
             if (consequencesIndex === -1) {
                 return [...state, action.payload]
             }
